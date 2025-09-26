2200ba3981bee21755b42d97031aea25cf0ba5fc
syota0304 <syota0304@gmail.com>
2025-09-26 19:14:43 +0900

    gh-pages のインストールと設定。

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: '/video_ocr/',
+    plugins: [react()],
     define: {
         'process.env.NODE_ENV': JSON.stringify('development'), // or 'development'
         'process.env': {} // 最低限必要
